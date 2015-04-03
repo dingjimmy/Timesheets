df756f4d4db7d4a635af9df1cf720f875bddb701
James Dingle <james.dingle@eyeplan.co.uk>
2015-04-03 23:38:18 +0100

    Create basic domain model.

---
+++ code/Timesheets.Data/Timesheet.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Timesheets.Data
+{
+    public class Timesheet
+    {
+        public int ID { get; set; }
+
+        public string Customer { get; set; }
+
+        public string Employee { get; set; }
+
+        public DateTime PeriodStarts { get; set; }
+
+        public DateTime PeriodEnds { get; set; }
+
+        public ICollection<TimesheetEntry> Entries { get; set; }
+        
+    }
+}
---
+++ code/Timesheets.Data/TimesheetEntry.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Timesheets.Data
+{
+    public class TimesheetEntry
+    {
+        public int ID { get; set; }
+
+        public DateTime StartedOn { get; set; }
+
+        public DateTime CompletedOn { get; set; }
+
+        public string Description { get; set; }
+    }
+}
