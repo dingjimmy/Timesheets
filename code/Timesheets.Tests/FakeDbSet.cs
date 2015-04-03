10bf4f9ecd2753c0937d77cbf5af0ac28cde2365
James Dingle <james.dingle@eyeplan.co.uk>
2015-04-04 00:12:44 +0100

    Create empty unit testing project.

---
+++ code/Timesheets.Tests/FakeDbSet.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Entity;
+using System.Collections.ObjectModel;
+
+namespace Timesheets.Tests
+{
+    class FakeDbSet<T> : IDbSet<T> where T : class
+    {
+        ObservableCollection<T> _data;
+        IQueryable _query;
+
+        public FakeDbSet()
+        {
+            _data = new ObservableCollection<T>();
+            _query = _data.AsQueryable();
+        }
+
+        public T Add(T entity)
+        {
+            _data.Add(entity);
+            return entity;
+        }
+
+        public T Attach(T entity)
+        {
+            _data.Add(entity);
+            return entity;
+        }
+
+        public TDerivedEntity Create<TDerivedEntity>() where TDerivedEntity : class, T
+        {
+            return Activator.CreateInstance<TDerivedEntity>();
+        }
+
+        public T Create()
+        {
+            return Activator.CreateInstance<T>();
+        }
+
+        public T Find(params object[] keyValues)
+        {
+            throw new NotImplementedException($"Derive from {nameof(FakeDbSet<T>)} and override {nameof(FakeDbSet<T>.Find)}");
+        }
+
+        public System.Collections.ObjectModel.ObservableCollection<T> Local
+        {
+            get { return _data; }
+        }
+
+        public T Remove(T entity)
+        {
+            _data.Remove(entity);
+            return entity;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return _data.GetEnumerator();
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return _data.GetEnumerator();
+        }
+
+        public Type ElementType
+        {
+            get { return _query.ElementType; }
+        }
+
+        public System.Linq.Expressions.Expression Expression
+        {
+            get { return _query.Expression; }
+        }
+
+        public IQueryProvider Provider
+        {
+            get { return _query.Provider; }
+        }
+    }
+}
