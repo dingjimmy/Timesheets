9268fa6dc91148b4ab433ef9af51b7d15d6c7cb6
James Dingle <james.dingle@eyeplan.co.uk>
2015-04-04 01:50:54 +0100

    Create caliburn.micro boilerplate - Added root ViewModel & View - Created caliburn bootrapper - Integrated ninject kernel into bootstrapper - Configured app.xaml to fireup bootstrapper.

---
+++ code/Timesheets.WinClient/ViewModels/RootViewModel.cs
@@ -0,0 +1,38 @@
+﻿using Caliburn.Micro;
+using System.Windows;
+using Timesheets.Data;
+
+namespace Timesheets.WinClient
+{
+    public class RootViewModel : PropertyChangedBase
+    {
+        string name;
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                NotifyOfPropertyChange(() => Name);
+                NotifyOfPropertyChange(() => CanSayHello);
+            }
+        }
+
+        public bool CanSayHello
+        {
+            get { return !string.IsNullOrWhiteSpace(Name); }
+        }
+
+
+        public RootViewModel(ITimesheetDbContext dbContext)
+        {
+            var ctx = dbContext;
+        }
+
+        public void SayHello()
+        {
+            MessageBox.Show(string.Format("Hello {0}!", Name)); //Don't do this in real life :)
+        }
+    }
+}
