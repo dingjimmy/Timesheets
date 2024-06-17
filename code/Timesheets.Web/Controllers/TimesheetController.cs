45f6fe147c0576869b00aea291d59bda3c9c52b0
James Dingle <ding_jimmy@hotmail.com>
2024-06-17 22:34:11 +0100

    refactor: mvc controller code now builds

--- code/Timesheets.Web/Controllers/TimesheetController.cs
+++ code/Timesheets.Web/Controllers/TimesheetController.cs
@@ -1,90 +1,92 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Text.Encodings.Web;
 
-public class TimesheetController : Controller
+namespace Timesheets.Web.Controllers
 {
-    private static readonly TimesheetValidator _Validator = new TimesheetValidator();
 
-    static readonly Dictionary<int, Timesheet> _Timesheets = new Dictionary<int, Timesheet>
+    public class TimesheetController : Controller
     {
-        { 1001, new Timesheet(1001, "2024 Week 5", "HM Government", "Winston Churchill", DateTime.Now.ToString(), DateTime.Now.AddDays(1).ToString()) },
-        { 1002, new Timesheet(1002, "2024 Week 6", "HM Government", "Winston Churchill", DateTime.Now.ToString(), DateTime.Now.AddDays(1).ToString()) },
-        { 1003, new Timesheet(1003, "2024 Week 7", "HM Government", "Winston Churchill", DateTime.Now.ToString(), DateTime.Now.AddDays(1).ToString()) }
-    };
+        private static readonly TimesheetValidator _Validator = new();
 
-    [HttpGet("timesheets")]
-    public IActionResult ListTimesheets()
-    {
-        return View("ListTimesheets");
-    }
-
-    [HttpGet("timesheet/{id}")]
-    public ActionResult ViewTimesheet(int id, bool? edit)
-    {
-        if (!_Timesheets.TryGetValue(id, out Timesheet? timesheet))
+        static readonly Dictionary<int, Timesheet> _Timesheets = new()
         {
-            return NotFound();
+            { 1001, new Timesheet(1001, "2024 Week 5", "HM Government", "Winston Churchill", DateTime.Now.ToString(), DateTime.Now.AddDays(1).ToString()) },
+            { 1002, new Timesheet(1002, "2024 Week 6", "HM Government", "Winston Churchill", DateTime.Now.ToString(), DateTime.Now.AddDays(1).ToString()) },
+            { 1003, new Timesheet(1003, "2024 Week 7", "HM Government", "Winston Churchill", DateTime.Now.ToString(), DateTime.Now.AddDays(1).ToString()) }
+        };
+
+        [HttpGet("timesheets")]
+        public IActionResult ListTimesheets()
+        {
+            return View();
         }
 
-        var viewToReturn = edit == true ? "EditTimesheet" : "ViewTimesheet";
-        return View(viewToReturn, timesheet);
-    }
-
-    [HttpGet("timesheet")]
-    public ActionResult NewTimesheet()
-    {
-        return View();
-    } 
-
-    [HttpPost("timesheet")]
-    public ActionResult SaveNewTimesheet(AddTimesheetRequest request)
-    {
-        if (!_Validator.IsValid(request.Name, request.Customer, request.Employee, request.PeriodStarts, request.PeriodEnds))
+        [HttpGet("timesheet")]
+        public ActionResult NewTimesheet()
         {
-            return BadRequest();
+            return View();
         }
 
-        try
+        [HttpGet("timesheet/{id}")]
+        public ActionResult ViewTimesheet(int id, bool? edit)
         {
-            var nextId = _Timesheets.Last().ID + 1;
-            var timesheet = new Timesheet(nextId, request.Name, request.Customer, request.Employee, request.PeriodStarts, request.PeriodEnds);
-            _Timesheets.Add(timesheet);
+            if (!_Timesheets.TryGetValue(id, out Timesheet? timesheet))
+            {
+                return NotFound();
+            }
 
-            return ViewTimesheet(timesheet.ID);
-        }
-        catch
-        {
-            //TODO: log error
-            return StatusCode(StatusCodes.Status500InternalServerError);
-        }
-    }
-
-    [HttpPut("timesheet/{id}")]
-    public ActionResult SaveEditedTimesheet(int id, UpdateTimesheetRequest request)
-    {
-        if (!_Validator.IsValid(request.Name, request.Customer, request.Employee, request.PeriodStarts, request.PeriodEnds))
-        {
-            return Results.BadRequest();
-        }
-        
-        if (!_Timesheets.Any(s => s.ID == id))
-        {
-            return Results.NotFound();
+            var viewToReturn = edit == true ? "EditTimesheet" : "ViewTimesheet";
+            return View(viewToReturn, timesheet);
         }
 
-        try
+        [HttpPost("timesheet")]
+        public ActionResult SaveNewTimesheet(AddTimesheetRequest request)
         {
-            var timesheet = new Timesheet(id, request.Name, request.Customer, request.Employee, request.PeriodStarts, request.PeriodEnds);
-            var idx = _Timesheets.FindIndex(ts => ts.ID == id);
-            _Timesheets[idx] = timesheet;
+            if (!_Validator.IsValid(request.Name, request.Customer, request.Employee, request.PeriodStarts, request.PeriodEnds))
+            {
+                return BadRequest();
+            }
 
-            return Results.Ok();
+            try
+            {
+                var nextId = _Timesheets.Keys.Last() + 1;
+                var timesheet = new Timesheet(nextId, request.Name, request.Customer, request.Employee, request.PeriodStarts, request.PeriodEnds);
+                _Timesheets.Add(nextId, timesheet);
+
+                return ViewTimesheet(timesheet.ID, false);
+            }
+            catch
+            {
+                //TODO: log error
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
         }
-        catch     
+
+        [HttpPut("timesheet/{id}")]
+        public ActionResult SaveEditedTimesheet(int id, UpdateTimesheetRequest request)
         {
-            //TODO: log error
-            return Results.StatusCode(StatusCodes.Status500InternalServerError);
+            if (!_Validator.IsValid(request.Name, request.Customer, request.Employee, request.PeriodStarts, request.PeriodEnds))
+            {
+                return BadRequest();
+            }
+
+            if (!_Timesheets.ContainsKey(id))
+            {
+                return NotFound();
+            }
+
+            try
+            {
+                var timesheet = new Timesheet(id, request.Name, request.Customer, request.Employee, request.PeriodStarts, request.PeriodEnds);
+                _Timesheets[id] = timesheet;
+
+                return ViewTimesheet(id, false);
+            }
+            catch
+            {
+                //TODO: log error
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
         }
-        return View();
     }
 }
