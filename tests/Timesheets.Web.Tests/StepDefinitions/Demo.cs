339415281cc6e2eef83bb18bef363e2e3e8ce014
James Dingle <ding_jimmy@hotmail.com>
2025-01-20 01:07:48 +0000

    chore: setup Reqnroll for E2E tests

---
+++ tests/Timesheets.Web.Tests/StepDefinitions/Demo.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.RegularExpressions;
+using Microsoft.Playwright;
+using Reqnroll;
+
+namespace Timesheets.Tests.StepDefinitions;
+
+[Binding]
+public class Demo(IPage page) : PlaywrightStepDefinitions(page)
+{
+    [Given("user has navigated to playwright home page")]
+    public async Task GivenUserHasNavigatedToPlaywrightHomePage()
+    {
+        await Page.GotoAsync("https://playwright.dev");
+    }
+
+    [When("they click on the 'get started' link")]
+    public async Task WhenTheyClickOnTheGetStartedLink()
+    {
+        // Click the get started link.
+        await Page.GetByRole(AriaRole.Link, new() { Name = "Get started" }).ClickAsync();
+    }
+
+    [Then("the page title must contain the word 'playwright'")]
+    public async Task ThenThePageTitleMustContainTheWordPlaywright()
+    {
+        // Expect a title "to contain" a substring.
+        await Expect(Page).ToHaveTitleAsync(new Regex("Playwright"));
+    }
+
+    [Then("the 'Installation' page is loaded")]
+    public async Task ThenTheInstallationPageIsLoaded()
+    {
+        // Expects page to have a heading with the name of Installation.
+        await Expect(Page.GetByRole(AriaRole.Heading, new() { Name = "Installation" })).ToBeVisibleAsync();
+    }
+}
---
+++ tests/Timesheets.Web.Tests/StepDefinitions/PlaywrightHooks.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Playwright;
+using Microsoft.Playwright.TestAdapter;
+using Reqnroll;
+using Reqnroll.BoDi;
+
+namespace Timesheets.Tests.StepDefinitions;
+
+[Binding]
+public class PlaywrightHooks
+{
+    private readonly IObjectContainer _Container;
+    
+    public PlaywrightHooks(IObjectContainer container)
+    {
+        _Container = container;
+    }
+
+    [BeforeFeature]
+    public static async Task SetupBrowser(FeatureContext featureContext)
+    {
+        // setup playwright
+        var playwright = await Playwright.CreateAsync();
+        playwright.Selectors.SetTestIdAttribute("data-testid");
+        var browserName = PlaywrightSettingsProvider.BrowserName;
+        featureContext["Playwright"] = playwright;
+        
+        // setup browser
+        var browser = await playwright[browserName].LaunchAsync();
+        featureContext["Browser"] = browser;
+    }
+
+    [BeforeScenario]
+    public async Task SetupBrowserContext(FeatureContext featureContext)
+    {
+        var browser = featureContext["Browser"] as IBrowser;
+        var browserContext = await browser!.NewContextAsync();
+        
+        var page = await browserContext.NewPageAsync();
+        _Container.RegisterInstanceAs(page);
+    }
+
+    [AfterFeature]
+    public static async Task TearDownBrowser(FeatureContext featureContext)
+    {
+        if (featureContext["Browser"] is IBrowser browser)
+        {
+            foreach (var context in browser.Contexts)
+            {
+                await context.CloseAsync().ConfigureAwait(false);
+            }
+            
+            await browser.CloseAsync().ConfigureAwait(false);
+            featureContext["Browser"] = null;
+        }
+
+        if (featureContext["Playwright"] is IPlaywright playwright)
+        {
+            playwright.Dispose();
+            featureContext["Playwright"] = null;
+        }
+    }
+}
