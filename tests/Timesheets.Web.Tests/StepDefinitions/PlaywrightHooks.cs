d9289135af5323cf8f1c861812fb942d2697672b
James Dingle <ding_jimmy@hotmail.com>
2025-01-20 16:29:21 +0000

    refactor: E2E tests use a different URL per environment.

--- tests/Timesheets.Web.Tests/StepDefinitions/PlaywrightHooks.cs
+++ tests/Timesheets.Web.Tests/StepDefinitions/PlaywrightHooks.cs
@@ -8,6 +8,9 @@ namespace Timesheets.Tests.StepDefinitions;
 [Binding]
 public class PlaywrightHooks
 {
+    private const string BASE_URL_LOCALDEV = "https://localhost:7133";
+    private const string BASE_URL_STAGING = "https://playwright.dev";
+    private const string BASE_URL_PRODUCTION = "https://playwright.dev";
     private readonly IObjectContainer _Container;
     
     public PlaywrightHooks(IObjectContainer container)
@@ -23,7 +26,7 @@ public class PlaywrightHooks
         playwright.Selectors.SetTestIdAttribute("data-testid");
         var browserName = PlaywrightSettingsProvider.BrowserName;
         featureContext["Playwright"] = playwright;
-        
+
         // setup browser
         var browser = await playwright[browserName].LaunchAsync();
         featureContext["Browser"] = browser;
@@ -33,7 +36,20 @@ public class PlaywrightHooks
     public async Task SetupBrowserContext(FeatureContext featureContext)
     {
         var browser = featureContext["Browser"] as IBrowser;
-        var browserContext = await browser!.NewContextAsync();
+        
+        var options = new BrowserNewContextOptions()
+        {
+            BaseURL = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") switch
+            {
+                "Development" => BASE_URL_LOCALDEV,
+                "Staging" => BASE_URL_STAGING,
+                "Production" => BASE_URL_PRODUCTION,
+                _ => string.Empty
+            }
+        };
+        
+        var browserContext = await browser!.NewContextAsync(options);
+        _Container.RegisterInstanceAs(browserContext);
         
         var page = await browserContext.NewPageAsync();
         _Container.RegisterInstanceAs(page);
